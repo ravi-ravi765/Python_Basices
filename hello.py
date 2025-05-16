7d85044df1d6985f99537a30ffde6c5b732b109e
Ravi <Ravitejat765@gmail.com>
2025-05-16 21:17:42 +0530

    basic and first file in python

--- hello.py
+++ hello.py
@@ -1,13 +1,13 @@
 # Basic python lines to print "Hello "
 
 def say_hello(input):
-	if input == "Hi":
+	if input in ["Ravi","Roman","Brock"]:
 		return f'Hello my friend {input}'
 	else:
-	    	return f'Hey Hii'
+	    return f'Hey Hii'
 	    	
-if "__name__" == "__main__":
-	user = input("Enter your name hear : ")
+if __name__ == "__main__":
+	user = "Brock"
 	
 	reply = say_hello(user)
 	print(reply)
